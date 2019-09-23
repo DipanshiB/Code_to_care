0a7f6f5267581ec5ce43c5203348c4761e2381ce
Dipanshi Bansal <dipanshibansal@gmail.com>
2019-09-23 20:01:10 +0530

    changes committed

---
+++ index.js
@@ -0,0 +1,17 @@
+var firebaseConfig = {
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appID: "app-id",
+};
+
+service firebase.storage {
+  match /b/{bucket}/o {
+    match /{allPaths=**} {
+      allow read, write: if request.auth != null;
+    }
+  }
+}
